457980969bc90f886e7c4bf1063e99bc7c3e562e
Diana <diana.dev777@gmail.com>
2025-06-11 10:45:13 +0500

    create product page

--- src/pages/product-page/ProductPage.tsx
+++ src/pages/product-page/ProductPage.tsx
@@ -1,7 +1,57 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ProductType } from '../../types/Product-type';
+import s from './Product.module.scss';
+
 const ProductPage = () => {
+  const { id } = useParams();
+  console.log(id);
+  const [product, setProduct] = useState<ProductType>({
+    _id: 0,
+    title: '',
+    description: '',
+    price: 0,
+    image: '',
+    category: '',
+    rating: 0,
+  });
+
+  useEffect(() => {
+    const getProductById = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/products/${id}`);
+        let cardImage = '/clothes.png';
+        const apiURL = 'http://localhost:8000';
+        if (response.data.image) {
+          cardImage = apiURL + '/uploads/' + response.data.image;
+          response.data.image = cardImage;
+        }
+        setProduct(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProductById();
+  }, [id]);
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className='container'>
+      <p>{product.title}</p>
+      <div className={s.card}>
+        <div>
+          <img src={product.image}></img>
+        </div>
+
+        <div>
+          <p>{product.price}</p>
+          <p>{product.rating}</p>
+
+          <button>1</button>
+          <button>add to card</button>
+          <button>favorite</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,21 +1,67 @@
-import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import s from './Register.module.scss';
 
 const RegisterPage = () => {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [inputType, setInputType] = useState('password');
+
+  const navigate = useNavigate();
+
+  const submitFormHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        alert('Passwords do not match');
+        return;
+      }
+      if (fullname && email && password) {
+        const newUser = {
+          username: fullname,
+          email,
+          password,
+        };
+
+        const response = await axios.post('http://localhost:8000/users', newUser);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', JSON.stringify(response.data.user));
+        navigate('/');
+      } else {
+        alert('Please fill in all fields');
+      }
+    } catch (error) {
+      alert('Something went wrong. Please try again later.');
+      console.error(error);
+    }
+  };
+
+  const inputTypeHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (inputType === 'password') {
+      setInputType('text');
+    } else {
+      setInputType('password');
+    }
+  };
+
   return (
     <div>
       <p className={s.title}>Sign up</p>
       <p className={s.text}>
         Registration takes less than a minute but gives you full control over your orders.
       </p>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label className={s.label}>Full Name</label>
           <input
             className={s.input}
             type='text'
-            // value={title}
-            // onChange={e => setTitle(e.target.value)}
+            value={fullname}
+            onChange={e => setFullname(e.target.value)}
           />
         </div>
         <div>
@@ -23,23 +69,35 @@ const RegisterPage = () => {
           <input
             className={s.input}
             type='text'
-            // value={title}
-            // onChange={e => setTitle(e.target.value)}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
-        <div>
+        <div style={{ position: 'relative' }}>
+          <button className={s.eye} onClick={inputTypeHandler}>
+            {inputType === 'password' ? (
+              <img src='/closedEye.svg' alt='closedEye' style={{ width: '16px', height: '16px' }} />
+            ) : (
+              <img src='/Eyes.svg' alt='openedEye' style={{ width: '16px', height: '16px' }} />
+            )}
+          </button>
           <label className={s.label}>Password</label>
           <input
             className={s.input}
-            type='text'
-            // value={price}
-            // onChange={e => setPrice(Number(e.target.value))}
+            type={inputType}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
         <div>
           <label className={s.label}>Confirm Password</label>
-          <input className={s.input} type='text' />
+          <input
+            className={s.input}
+            type={inputType}
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
         </div>
 
         <button className={s.saveButton} type='submit'>
