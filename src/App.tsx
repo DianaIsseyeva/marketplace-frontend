ecefa1ad3d6ba12e75bd369d1945d27b059dcccc
Diana <diana.dev777@gmail.com>
2025-06-13 10:50:31 +0500

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { store } from '../store';
 import './App.scss';
 import Layout from './layout/Layout';
 import CatalogPage from './pages/catalog-page/CatalogPage';
@@ -9,18 +11,20 @@ import SignInPage from './pages/sign-in/SignIn';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path='/catalog' element={<CatalogPage />} />
-          <Route path='/catalog/:category' element={<CatalogPage />} />
-          <Route path='/catalog/product/:id' element={<ProductPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<SignInPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/catalog' element={<CatalogPage />} />
+            <Route path='/catalog/:category' element={<CatalogPage />} />
+            <Route path='/catalog/product/:id' element={<ProductPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/login' element={<SignInPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
